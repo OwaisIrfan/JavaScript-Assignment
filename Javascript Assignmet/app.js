ff9a332ae16732043b402533b584af6ffb571547
OwaisIrfan <65021720+OwaisIrfan@users.noreply.github.com>
2020-06-12 12:27:08 +0500

    Add files via upload

---
+++ Javascript Assignmet/app.js
@@ -0,0 +1,90 @@
+// chapter1
+
+
+// (Task#1)
+// alert('Error! Please enter valid password')
+
+
+
+// (Task#2)
+// alert('Welcome to JS Land\nHappy Coding!')
+
+
+// (Task#3)
+// alert('Welcome to JS Land')
+// alert('Prevent this page from creating additional dialogs.')
+
+
+// (Task#4)
+// var a = ('Hello... I can run JS thorough my web browser console');
+// alert (a); 
+// This code should be paste in the console for the result
+
+
+
+
+
+// chapter2
+
+
+
+
+// (Task#1)
+// var username = task1
+
+
+// (Task#2)
+// var myName = ('john')
+
+// (Task#3)
+// var titledmessage = ('Hello World');
+// alert(titledmessage);
+
+// (Task#4)
+// var a = ('jhone doe');
+// var b = ('15 years old');
+// var c = ('Certified Mobile Application Development');
+// alert(a);
+// alert(b);
+// alert(c);
+
+// (Task#5)
+// alert(' PIZZA \n PIZZ \n PIZ \n PI \n P');
+
+// (Task#6)
+// var Email = ('My Email address is')
+// var a = ('example@example.com')
+// alert(Email + " " + a)
+
+// (Task#7)
+// var book = ('I am trying to learn from the Book A smarter way to learn JavaScript')
+// alert (book);
+
+// (Task#8)
+// var a = ('Yah! I can write HTML content through JavaScript');
+// document.write(a);
+
+// (Task#9)
+// var a =('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬');
+// alert(a);
+// document.write(a);
+
+
+
+
+// chapter3
+
+
+
+
+// (Task#1)
+// var age = ('I am 15 years old')
+// alert(age)
+
+
+// (Task#2)
+
+
+// (Task#3)
+
+
